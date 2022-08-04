d75aa74fceb4a216217131a22c7a47e52e778b00
pyneng <pyneng.course@gmail.com>
2022-08-04 05:46:34 +0000

    task table

---
+++ task_01.py
@@ -0,0 +1,20 @@
+"""
+Задание 1.1
+
+Если запустить код задания, будет такой вывод:
+$ python task_1_1.py
+line1
+line2
+line3
+
+Надо преобразовать строку start_data таким образом, чтобы на экран была выведена
+такая строка
+$ python task_1_1.py
+line1 line2 line3
+
+При этом нельзя менять строку start_data вручную, то есть нельзя менять исходную строку
+start_data.
+"""
+start_data = "line1\nline2\nline3"
+print(start_data)
+
