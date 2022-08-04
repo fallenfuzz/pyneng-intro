5109386900981bd45140a3a6ff5a15817ae03eaa
pyneng <pyneng.course@gmail.com>
2022-08-04 13:36:44 +0000

    tasks 1-3

---
+++ task_03.py
@@ -0,0 +1,21 @@
+"""
+Если запустить код задания, будет такой вывод:
+$ python task_03.py
+Python is a high-level, interpreted, general-purpose programming language.
+
+Получить из строки start_data такой список (удалены запятые и точка и строка
+разделена на слова):
+['Python', 'is', 'a', 'high-level', 'interpreted', 'general-purpose', 'programming', 'language']
+
+Полученный список вывести на экран с помощью print:
+$ python task_03.py
+['Python', 'is', 'a', 'high-level', 'interpreted', 'general-purpose', 'programming', 'language']
+
+Тут очень важный момент, что надо получить именно список (тип данных), а не,
+например, строку, которая похожа на показанный список.
+
+Ограничение: исходную строку start_data нельзя менять вручную.
+"""
+start_data = "Python is a high-level, interpreted, general-purpose programming language."
+print(start_data)
+
