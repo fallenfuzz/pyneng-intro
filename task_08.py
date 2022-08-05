44f52609981963d8bc9f3c2fc1e194de88850425
pyneng <pyneng.course@gmail.com>
2022-08-05 08:08:52 +0000

    intro tasks update

---
+++ task_08.py
@@ -0,0 +1,27 @@
+"""
+Если запустить код задания, будет такой вывод:
+$ python task_08.py
+.py
+.pyi
+.pyc
+.pyd
+.pyw
+.pyz
+
+Надо изменить код так, чтобы при запуске кода был такой вывод
+$ python task_08.py
+script.py
+script.pyi
+script.pyc
+script.pyd
+script.pyw
+script.pyz
+
+При этом добавление слова script должно выполняться именно через переменную
+filename и нельзя менять список python_extensions.
+"""
+filename = "script"
+python_extensions = ['.py', '.pyi', '.pyc', '.pyd', '.pyw', '.pyz']
+for ext in python_extensions:
+    print(ext)
+
